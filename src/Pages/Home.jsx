71852a8a4078cea584dd1a7cbbd0f4e98e10f601
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-23 18:21:07 +0530

    feat: add smooth scroll with useRef and integrate application preview component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,18 +3,38 @@ import { useState } from "react";
 import Navbar from "../Components/Navbar"
 import Hero from "../Components/Hero";
 import ApplicationForm from "../Components/ApplicationForm";
+import ApplicationPreview from "../Components/ApplicationPreview";
+import Feature from "../Components/Feature";
+import { useRef } from "react";
+
 
 const Home = () => {
     const [isFormOpen, setIsFormOpen] = useState(false)
+    const [applicationPreview, setApplicationPreview] = useState("")
+    const formRef = useRef(null);
+
+    const handleScroll = () => {
+        setTimeout(() => {
+            formRef.current?.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        }, 100);
+    };
+
     return (
         <>
 
             <Navbar />
-            <div className="relative h-180 pb-10">
-                <Hero setIsFormOpen={setIsFormOpen} />
+            <div ref={formRef} className={`relative ${applicationPreview ? "h-210" : isFormOpen ? "h-180" : "h-100"} pb-10`}>
+                <Hero handleScroll={handleScroll} setIsFormOpen={setIsFormOpen} />
 
-                <ApplicationForm isFormOpen={isFormOpen} setIsFormOpen={setIsFormOpen} />
+                <ApplicationForm isFormOpen={isFormOpen} setApplicationPreview={setApplicationPreview} setIsFormOpen={setIsFormOpen} />
+                <ApplicationPreview setIsFormOpen={setIsFormOpen} applicationPreview={applicationPreview} setApplicationPreview={setApplicationPreview} />
+                <Feature />
             </div>
+
+
         </>
     )
 }
