cd01f3dd0de553c77504b4504f55090b05acbfa0
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-23 18:36:03 +0530

    style: change the z-index of Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import { FaFileAlt } from "react-icons/fa";
 
 const Navbar = () => {
     return (
-        <nav className="fixed top-0 w-full px-5 bg-blue-500  z-50">
+        <nav className="fixed top-0 w-full px-5 bg-blue-500  z-60">
             <div className="w-full flex items-center gap-2.5 min-[450px]:gap-4  py-5">
                 <div className="flex items-center">
                     <FaFileAlt className="text-white text-3xl min-[565px]:text-4xl" />
