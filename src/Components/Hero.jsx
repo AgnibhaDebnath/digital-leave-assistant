c2e3b71ac075c7ee3218372f998712a5c597c66c
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-23 18:33:38 +0530

    style: update hero subheading and improve CTA button styling

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,15 +1,24 @@
-const Hero = ({ setIsFormOpen }) => {
+
+
+const Hero = ({ setIsFormOpen, handleScroll }) => {
+
     return (
-        <section className="mt-30 flex justify-center flex-col items-center ">
+
+        <section className="mt-50 flex justify-center flex-col items-center ">
             <div className="py-2 px-1">
-                <h1 className="max-[374px]:text-[1.4rem] text-[1.45rem] min-[490px]:text-3xl min-[600px]:text-4xl min-[900px]:text-5xl font-bold font-[Inter] tracking-wide text-gray-900">Easy Leave Application Generator</h1>
+                <h1 className="max-[384px]:text-[1.4rem] text-[1.45rem] min-[490px]:text-3xl min-[600px]:text-4xl min-[900px]:text-5xl font-bold font-[Inter] tracking-wide text-gray-900">Easy Leave Application Generator</h1>
             </div>
             <div className="flex flex-col gap-3  items-center">
-                <div>
-                    <p className="text-lg font-[Inter] tracking-wide text-gray-500 ">For Anganwadi Workers</p>
+                <div className="flex justify-center mt-4">
+                    <h2 className=" text-sm min-[507px]:text-[1rem] min-[570px]:text-lg text-gray-600 font-[inter] text-center">
+                        Create professional leave applications instantly for Anganwadi workers
+                    </h2>
                 </div>
                 <div>
-                    <button onClick={() => setIsFormOpen(true)} className="px-2.5 py-1 rounded-md bg-green-100 hover:bg-green-200 text-green-500 cursor-pointer hover:scale-105 transition duration-200 font-medium font-[Inter]">Start now</button>
+                    <button onClick={() => {
+                        setIsFormOpen(true)
+                        handleScroll()
+                    }} className="px-10 py-1.5 rounded-lg hover:bg-green-600  bg-green-500 text-white cursor-pointer hover:scale-105 transition text-[1.1rem] duration-200 font-medium font-[Inter]">Start Now</button>
                 </div>
             </div>
         </section>
