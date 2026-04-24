038059fb7459a7423f789893f1b8581ca7b35182
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-04-24 13:08:00 +0530

    style: adjust responsive breakpoints for better layout

--- src/Components/ApplicationPreview.jsx
+++ src/Components/ApplicationPreview.jsx
@@ -17,7 +17,7 @@ const ApplicationPreview = ({ applicationPreview, setIsFormOpen, setApplicationP
     })
     return (
         <>
-            {applicationPreview && <div className="w-86 z-50 min-[430px]:w-100 min-[513px]:w-120 min-[600px]:w-140 min-[683px]:w-160 min-[770px]:w-180 mx-auto mt-10 p-6 bg-white shadow-xl rounded-xl text-sm leading-relaxed absolute top-16 min-[600px]:top-18 left-1/2 transform -translate-x-1/2">
+            {applicationPreview && <div className="w-86 z-50 min-[430px]:w-100 min-[513px]:w-120 min-[600px]:w-140 min-[683px]:w-160 min-[770px]:w-180 mx-auto mt-3  p-6 bg-white shadow-xl rounded-xl text-sm leading-relaxed absolute max-[416px]:top-25 top-21 min-[570px]:top-23 min-[600px]:top-25 left-1/2 transform -translate-x-1/2">
 
                 <h3 className="text-2xl font-semibold text-center mb-3 font-[inter]">
                     Application Preview
@@ -25,7 +25,7 @@ const ApplicationPreview = ({ applicationPreview, setIsFormOpen, setApplicationP
                 <div className="flex justify-end ">
                     <p className="font-[inter]">Date:{application_date}</p>
                 </div>
-                <pre className="whitespace-pre-wrap font-[inter]">
+                <pre className="whitespace-pre-wrap font-[inter] mt-1">
 
                     {applicationPreview}
                 </pre>
